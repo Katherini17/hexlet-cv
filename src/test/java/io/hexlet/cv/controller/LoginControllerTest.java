ed52a07d50c6014a5f80cd82abf17c2d2f340c00
textile86 <textile2006@list.ru>
2026-07-27 18:26:01 +0300

    Add test profile with production cookie and JWT settings [issue 1215]

--- src/test/java/io/hexlet/cv/controller/LoginControllerTest.java
+++ src/test/java/io/hexlet/cv/controller/LoginControllerTest.java
@@ -25,12 +25,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class LoginControllerTest {
 
     @Autowired
