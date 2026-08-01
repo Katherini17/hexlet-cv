1b7fd72b0011a2cd641a4b83947cfa84fe77cf36
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-08-01 18:50:23 +0300

    refactor(model): добавить аннотации @Column для полей сущности StoriesStory

--- src/main/java/io/hexlet/cv/model/StoriesStory.java
+++ src/main/java/io/hexlet/cv/model/StoriesStory.java
@@ -25,21 +25,26 @@ public class StoriesStory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "author_name", nullable = false)
     private String authorName;
 
+    @Column(name = "avatar_url")
     private String avatarUrl;
 
+    @Column(name = "company_name")
     private String companyName;
 
+    @Column(name = "offer_position")
     private String offerPosition;
 
     @Column(name = "text", columnDefinition = "TEXT", nullable = false)
     private String text;
 
+    @Column(name = "display_order")
     private Integer displayOrder;
 
-    @Column(name = "is_published", nullable = false, columnDefinition = "boolean default false")
     @Builder.Default
+    @Column(name = "is_published", nullable = false, columnDefinition = "boolean default false")
     private Boolean isPublished = false;
 
 }
