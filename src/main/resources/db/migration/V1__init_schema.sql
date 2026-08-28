d7d8b7ef1ce11aecd89c7542eaa1e2f8e2420e93
Ekaterina Avdeenko <Katherini@yandex.ru>
2026-08-28 21:35:45 +0300

    fix(db): добавить ON DELETE CASCADE для fk_refresh_tokens_user

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -723,5 +723,5 @@ ALTER TABLE vacancies
 
 ALTER TABLE refresh_tokens
     ADD CONSTRAINT fk_refresh_tokens_user
-        FOREIGN KEY (user_id) REFERENCES users(id);
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
 
